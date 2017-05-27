856ead2c0de8be8c40e39d4c8a10101ac6eb9892
dev1 <dev1>
2017-05-27 23:26:54 +0300

    clean code

--- src/main/java/com/softserve/edu/task6/tickets/TicketProcessor.java
+++ src/main/java/com/softserve/edu/task6/tickets/TicketProcessor.java
@@ -15,26 +15,28 @@ public class TicketProcessor {
      * get file name with key word and
      * sum lucky tickets
      */
-    public void execute(){
+    public void execute() {
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
-        List<String> fileContent=null;
+        List<String> fileContent;
         try {
-            fileContent= Files.readAllLines(Paths.get(input));
+            fileContent = Files.readAllLines(Paths.get(input));
         } catch (IOException e) {
-            e.printStackTrace();
+            System.out.println(String.format("some error occurs when read file %s : %s", input, e
+                    .getMessage()));
             return;
         }
         String city = fileContent.get(0);
         int luckyTickets = 0;
         for (int i = 0; i < 1000000; i++) {
             Ticket ticket = TicketFactory.getInstance(city, i);
-            if (ticket!=null && ticket.isLucky()){
+            if (ticket != null && ticket.isLucky()) {
                 luckyTickets++;
-            } else if (ticket==null){
-                throw new IllegalArgumentException(String.format("city name %s is not correct",city));
+            } else if (ticket == null) {
+                System.out.println(String.format("city name %s is not correct", city));
+                return;
             }
         }
-        System.out.println(String.format("%s lucky tickets - %s",city,luckyTickets));
+        System.out.println(String.format("%s lucky tickets - %s", city, luckyTickets));
     }
 }
